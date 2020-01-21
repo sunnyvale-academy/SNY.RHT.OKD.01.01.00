aca39977fe4cdf18cab3adb9f0a5785fdf07cdbf
Denis Maggiorotto <denis.maggiorotto@sunnyvale.it>
2020-01-21 14:35:29 +0100

    initial import

---
+++ labs/01-Provision_the_environment/vagrant/3.x/scripts/oc-up-within.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# Copyright 2020 Denis Maggiorotto
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#!/bin/bash
+
+
+
+
+cp /vagrant/keys/master_pvt_key.pem ~/.ssh/id_rsa
+cp /vagrant/keys/master_pub_key.pem ~/.ssh/id_rsa.pub
+
+
+readonly openshift_release=`cat /vagrant/Vagrantfile | grep '^OPENSHIFT_RELEASE' | awk -F'=' '{print $2}' | sed 's/^[[:blank:]\"]*//;s/[[:blank:]\"]*$//'`
+
+. "/vagrant/scripts/common.sh"
+
+export OPENSHIFT_RELEASE=$openshift_release
+
+
+# Fix permission issue on Windows host (#13)
+chmod 600 /home/vagrant/.ssh/*.key
+
+if [ "$(version $openshift_release)" -gt "$(version 3.7)" ]; then
+    ansible-playbook --key-file "~/.ssh/id_rsa" -i /vagrant/ansible-hosts /home/vagrant/openshift-ansible/playbooks/prerequisites.yml && \
+    ansible-playbook --key-file "~/.ssh/id_rsa" -i /vagrant/ansible-hosts /home/vagrant/openshift-ansible/playbooks/deploy_cluster.yml
+else
+    ansible-playbook --key-file "~/.ssh/id_rsa" -i /vagrant/ansible-hosts /home/vagrant/openshift-ansible/playbooks/byo/config.yml
+fi
